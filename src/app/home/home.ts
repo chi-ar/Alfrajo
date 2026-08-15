fa6f8926be170099fa8d3022cd5f8e71fffa652e
Chiara Ciamberlano <chiaraciamberlano96@gmail.com>
2026-08-15 19:34:46 +0200

    fix player name for text box

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Game } from '../services/game';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -17,11 +16,9 @@ import { Game } from '../services/game';
 })
 export class Home {
 
-
   playersNumber = 1;
 
   life = 30;
-
 
   names: string[] = [
     '',
@@ -34,14 +31,48 @@ export class Home {
   constructor(
     private game: Game,
     private router: Router
-  ){}
+  ) {}
 
 
+  // ==========================================
+  // SELEZIONE NUMERO GIOCATORI
+  // ==========================================
 
-  start(){
+  selectPlayers(number: number) {
+
+    this.playersNumber = number;
+
+  }
+
+
+  // ==========================================
+  // TRACK DEGLI INPUT
+  // ==========================================
+
+  trackPlayer(index: number, player: string) {
+
+    return index;
+
+  }
+
+
+  // ==========================================
+  // INIZIA PARTITA
+  // ==========================================
+
+  start() {
 
     const selectedNames =
-      this.names.slice(0, this.playersNumber);
+      this.names
+        .slice(0, this.playersNumber)
+        .map((name, index) => {
+
+          const cleanName = name.trim();
+
+          return cleanName ||
+            `Giocatore ${index + 1}`;
+
+        });
 
 
     this.game.startGame(
